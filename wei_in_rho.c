59e570a40a90973f13f52e50a013aebed49d369a
Scott Liu <liuleeeeee@icloud.com>
2014-10-17 17:16:57 +0800

    Update wei_in_rho.c

--- wei_in_rho.c
+++ wei_in_rho.c
@@ -1,2 +1,17 @@
 // weight_value.c
 #include <stdio>
+void ask(void);
+
+void ask(void)
+{
+      printf("What's your weight?\n");
+}
+
+int main(void)
+{
+      float weight,value,price;
+      ask();
+      printf("My weight is :");
+      scanf("%f",&weight);
+      
+      

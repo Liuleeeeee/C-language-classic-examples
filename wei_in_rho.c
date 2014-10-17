4733a04d2d50ba30e76bb3e2adae38f89e2afeeb
Scott Liu <liuleeeeee@icloud.com>
2014-10-17 17:30:32 +0800

    Update wei_in_rho.c

--- wei_in_rho.c
+++ wei_in_rho.c
@@ -13,5 +13,9 @@ int main(void)
       ask();
       printf("My weight is :");
       scanf("%f",&weight);
-      
+      printf("\nAnd the price is :");
+      scanf("%f",&price);
+      value=weight*price*14.5833;
+      printf(Aha! Your weight in rhodium is worth $%.2f.\n",value);
+}
       
